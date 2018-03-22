89cc6dfe07cd844361a72f9a9281a6ba817d89b7
Jason Baker <usaman2010us@gmail.com>
2018-03-21 17:05:51 -0700

    Switch from gibberish/english to english/spanish/loremipsum

--- index.js
+++ index.js
@@ -18,50 +18,124 @@ const maxarg = (array) => {
 };
 
 const categories = {
-    GIBBERISH: 0,
+    SPANISH: 0,
     ENGLISH: 1,
+    LOREMIPSUM: 2,
 };
 
 const trainingMessages = {
-    GIBBERISH: [
-        'dfbijdf odfg nosfdg ',
-        'pgbmso lbfjlngfbjdnfgj kldfgbklfgbn',
-        'rogijjoeigj ergpmer kmdvbkldsmbdf',
-        'sdfgofsdnjgj jlndfsgnjksdfg nje4rtkjnertjkner',
-        'fdb dfbjmklsdfnb sdnsw ifhasduif',
-        ' idfpjgsaiod OIJDFSIOAG dfelgksafdg',
+    SPANISH: [
+        'Pienso que haya un error en la cuenta.',
+        'Os gustáis salie en balandro.',
+        'Tiene el pulgar hinchado.',
+        '¿Le queda alguna habitación por la noche?',
+        'Una abeja/una avispa me picó.',
+        'Pagar las entradas.',
+        'Él rompió la ventana.',
+        'Estoy arrepentido pero estamos completos.',
+        'Examinad la batería, por favor.',
+        '¿Hay plazas libres?',
+        '¿Tiene un sitio para una tienda?',
+        'Seguid esta calle / carretera.',
+        '¿Cúanto cuesta?',
+        '¿Cuántos años tenéis?',
+        '¿Podría preguntar a alguien a llevar mis maletas a mi habitación?',
     ],
     ENGLISH: [
-        'I am 12 what is this',
-        'Hello world.',
-        'Hi there how are you',
-        'Some day I will rule the world',
-        'I love food',
-        'Want to go over there, or over here?',
+        'May I sit next to you?',
+        'She has had quite a lot to drink.',
+        'You\'d better hurry up if you want to get home before dark.',
+        'Hi Kim.',
+        'You had better ask him in advance how much it will cost.',
+        'It\'s already nine o\'clock.',
+        'I must have lost it.',
+        'I think it\'s time for me to contact her.',
+        'He can come.',
+        'I don\'t know what Mary is looking for.',
+        'He turned around.',
+        'We\'ve arrived.',
+        'He left a large fortune to his son.',
+        'She decided to marry him even though her parents didn\'t want her to.',
+        'People don\'t say that anymore.',
+        'I\'ll buy a watch for my son.',
+        'I am no match for him.',
+        'I\'m in the car driving home.',
+        'Have you ever been mugged?',
+        'He helped me move.',
+        'It\'s next to that building.',
+        'She\'s about the same height as you.',
+        'He is a man of action.',
+        'Slip on your shoes.',
+        'I noticed that she was wearing new glasses.',
+        'How do you say that in Italian?',
+        'How long do you want it for?',
+        'Yes, that\'s right. You have a good memory.',
+        'I have a hunch that it will rain.',
+        'I wish he had attended the meeting.',
     ],
+    LOREMIPSUM: [
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+        'Aenean sagittis sapien a volutpat viverra',
+        'Etiam scelerisque massa eget nibh vehicula iaculis',
+        'Vestibulum nec mi placerat, consectetur turpis ac, dictum quam',
+        'Integer et turpis dictum, pharetra augue id, pharetra sapien',
+        'Pellentesque facilisis diam a lacinia lacinia',
+        'Sed in sem laoreet, ultricies velit sed, sagittis dui',
+        'Sed sit amet ex et mauris tincidunt eleifend',
+        'Nam auctor risus a ante imperdiet sagittis',
+        'Proin facilisis dolor vel nunc scelerisque, non hendrerit nibh laoreet',
+        'Cras ut ex sit amet odio consectetur ultrices vel placerat eros',
+        'Ut cursus mauris a mauris congue finibus',
+        'Aliquam in metus quis augue euismod vehicula nec a nibh',
+        'Ut at tortor ac turpis placerat lacinia',
+        'Mauris ullamcorper ex sit amet metus suscipit, ac dictum lorem pharetra',
+        'Aliquam ultrices felis id posuere fermentum',
+        'Mauris non nisl ac nisl ullamcorper egestas quis non nisi',
+        'Suspendisse eleifend diam a fringilla egestas',
+        'Quisque posuere magna at nunc aliquet, sed venenatis urna commodo',
+        'Donec quis massa at enim feugiat imperdiet',
+        'Proin pellentesque eros a tortor consectetur, eu tristique orci volutpat',
+        'Sed pharetra lorem et tellus facilisis varius vitae vitae tellus',
+        'Curabitur ut leo sit amet nunc lobortis facilisis nec quis sapien',
+        'Morbi ac diam eget erat pellentesque bibendum',
+        'Mauris sed sapien in augue vehicula imperdiet',
+        'Nulla nec nibh feugiat urna consectetur cursus',
+        'Morbi eu dui consectetur, sagittis nunc eu, dictum ex',
+        'Vivamus rhoncus quam et egestas fermentum',
+        'Vivamus sit amet nunc eu arcu porttitor euismod',
+        'Nam non quam efficitur, tincidunt eros ut, tristique metus',
+        'Donec porta arcu non lectus suscipit molestie',
+        'Nullam eget nibh maximus, scelerisque neque non, dapibus ex',
+    ]
 };
 
-const trainingTexts = mimir.dict(trainingMessages.GIBBERISH.concat(trainingMessages.ENGLISH));
+const trainingTexts = mimir.dict(trainingMessages.SPANISH.concat(trainingMessages.ENGLISH.concat(trainingMessages.LOREMIPSUM)));
 
 const trainingData = [];
-trainingMessages.GIBBERISH.forEach((gibberishMsg) => {
-    trainingData.push([mimir.bow(gibberishMsg, trainingTexts), categories.GIBBERISH]);
+trainingMessages.SPANISH.forEach((spanishMsg) => {
+    trainingData.push([mimir.bow(spanishMsg, trainingTexts), categories.SPANISH]);
 });
 trainingMessages.ENGLISH.forEach((englishMsg) => {
     trainingData.push([mimir.bow(englishMsg, trainingTexts), categories.ENGLISH]);
+});
+trainingMessages.LOREMIPSUM.forEach((LOREMIPSUMMsg) => {
+    trainingData.push([mimir.bow(LOREMIPSUMMsg, trainingTexts), categories.LOREMIPSUM]);
 });
 
 const neuralNetwork = new brain.NeuralNetwork();
 neuralNetwork.train(trainingData.map((pair) => {
     return {
         input: pair[0],
-        output: vectorResult(pair[1], 3),
+        output: vectorResult(pair[1], Object.keys(categories).length),
     };
 }));
 
 
-const resultEnglish = maxarg(neuralNetwork.run(mimir.bow('Some random crap here do you know what I mean', trainingTexts)));
+const resultEnglish = maxarg(neuralNetwork.run(mimir.bow('Would you like to dance?', trainingTexts)));
 console.log('Passing english msg, result is:', Object.keys(categories)[resultEnglish]); // ENGLISH
 
-const resultGibberish = maxarg(neuralNetwork.run(mimir.bow('awsfasdgf asdfa d 321432jlnssjksdf njsd', trainingTexts)));
-console.log('Passing gibberish msg:', Object.keys(categories)[resultGibberish]); // GIBBERISH
+const resultSpanish = maxarg(neuralNetwork.run(mimir.bow('¿Dónde ésta la parada de autobús más cerca?', trainingTexts)));
+console.log('Passing spanish msg:', Object.keys(categories)[resultSpanish]); // SPANISH
+
+const randomLoremIpsum = maxarg(neuralNetwork.run(mimir.bow('Nam quis velit nec urna consectetur vestibulum in blandit purus.', trainingTexts)));
+console.log('Passing lorem ipsum msg:', Object.keys(categories)[randomLoremIpsum]); // LOREMIPSUM
